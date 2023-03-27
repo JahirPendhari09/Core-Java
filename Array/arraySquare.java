59ca895a0299a7806cd198e4cb66b537bf92253f
JahirPendhari <jahirpp1999@gmail.com>
2023-03-27 06:59:11 +0530

    Printing Array Square in Ascending Order Solution

---
+++ Array/arraySquare.java
@@ -0,0 +1,19 @@
+// Array Square Print in Ascending Order 
+import java.util.*;
+public class arraySquare {
+    public static void printSquareSort(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            arr[i] = arr[i]* arr[i];
+        }
+        // Sort using Inbuilt Function 
+        Arrays.sort(arr);
+        // Printing Array 
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main (String args  []){
+        int arr [] = {-4,-1,0,3,10};
+        printSquareSort(arr);
+    }
+}
