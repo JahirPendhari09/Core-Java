122cf8db44b2a5a42303d5f14a8e3ce2c269dd72
JahirPendhari <jahirpp1999@gmail.com>
2023-03-31 11:07:08 +0530

    Printing Hollow Rhombus

---
+++ Patterns/hollow_Rhombus.java
@@ -0,0 +1,24 @@
+package Patterns;
+
+public class hollow_Rhombus {
+    public static void Rhombus(int n){
+        for (int i=1; i<=n; i++){
+            // spaces
+            for (int j=1; j<=n-i; j++){
+                System.out.print("  ");
+            }
+            // stars
+            for(int j=1; j<=n; j++){
+                if(i==1 || i==n || j==1|| j==n){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main (String[] args){
+        Rhombus(5);
+    }
+}
