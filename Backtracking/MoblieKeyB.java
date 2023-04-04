f5d532c04d96676ee0522f8b76804d03df1ecd54
JahirPendhari <jahirpp1999@gmail.com>
2023-04-04 10:20:03 +0530

    Mobile Key Search Program

---
+++ Backtracking/MoblieKeyB.java
@@ -0,0 +1,29 @@
+package BackTracking;
+
+public class MoblieKeyB {
+    final static char [][] L={{},{},{'a','b','c'},{'d','e','f'},{'g','h','i'},{'j','k','l'},{'m','n','o'},{'p','q','r','s'},{'t','u','v'},{'w','x','y','z'}};
+    
+    public static void letterCombination(String s){
+        int len = s.length();
+        if(len == 0){
+            System.out.println("");
+            return;
+        }
+        bfs(0, len,new StringBuilder(), s);
+       
+    }
+    public static void  bfs(int pos, int len, StringBuilder sb, String s){
+        if(pos ==len){
+            System.out.print(sb.toString()+" ");
+        }else{
+            char [] Letter =L[Character.getNumericValue(s.charAt(pos))];
+            for(int i=0; i<Letter.length; i++){
+                bfs(pos+1, len, new StringBuilder(sb).append(Letter[i]),s);
+            } 
+        }
+    }
+    public static void main (String args []){
+        letterCombination("8");
+        // Output is = t u v
+    }
+}
