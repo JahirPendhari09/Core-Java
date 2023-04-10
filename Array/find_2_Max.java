b88ff4902aa4ff7ea7e40ec2f3f530c01fbef095
JahirPendhari <jahirpp1999@gmail.com>
2023-04-10 16:38:28 +0530

    Find Second maximum in Array

---
+++ Array/find_2_Max.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class find_2_Max {
+    public static void main(String args []){
+        int arr [] = {7,70,87,54};
+        int max = Integer.MIN_VALUE;
+        int secMax = Integer.MIN_VALUE;
+
+        for(int i=0; i<arr.length; i++){
+            if(max<arr[i]){
+                secMax = max;
+                max = arr[i];
+            }
+        }
+        System.out.println("Maximum is :"+max);
+        System.out.println("2 Maximum is :"+secMax);
+    }
+}
