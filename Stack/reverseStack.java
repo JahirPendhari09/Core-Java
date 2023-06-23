5a58899cc120e28e5738b5eebb105c76007d2746
JahirPendhari <jahirpp1999@gmail.com>
2023-06-23 10:44:36 +0530

    Reverse a Stack

---
+++ Stack/reverseStack.java
@@ -0,0 +1,38 @@
+package Stack;
+import java.util.*;
+
+public class reverseStack {
+    public static void pushAtBottom(Stack<Integer> s, int data){
+        if(s.isEmpty()){
+            s.push(data);
+            return ;
+        }
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
+    public static void reversStack(Stack<Integer>s){
+        if(s.isEmpty()){
+            return ;
+        }
+        int top = s.pop();
+        reversStack(s);
+        pushAtBottom(s, top);
+
+    }
+    public static void printStack(Stack<Integer>s){
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+    public static void main (String args []){
+        Stack<Integer> s= new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        reversStack(s);
+        printStack(s);
+
+    }
+}
