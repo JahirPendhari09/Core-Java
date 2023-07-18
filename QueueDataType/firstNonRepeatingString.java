3f0ce094aa1e294bceea9ae89688f429d9730487
JahirPendhari <jahirpp1999@gmail.com>
2023-07-18 09:46:22 +0530

    Find First Non repeating element from string using Queue

---
+++ QueueDataType/firstNonRepeatingString.java
@@ -0,0 +1,30 @@
+package QueueDataType;
+import java.util.*;
+
+public class firstNonRepeatingString {
+    public static void nonFirstRepeatingS(String str){
+        int freq [] = new int [26];
+        Queue<Character> q = new LinkedList<>();
+
+        for(int i=0; i< str.length(); i++){
+            char ch = str.charAt(i);
+
+            q.add(ch);
+            freq[ch -'a'] ++;
+
+            while(!q.isEmpty() && freq[q.peek()-'a']> 1){
+                q.remove();
+            }
+
+            if(q.isEmpty()){
+                System.out.print("-1"+" ");
+            }else{
+                System.out.print(q.peek()+" ");
+            }
+        }
+    }
+    public static void main (String args []){
+        String str = "aabccxb";
+        nonFirstRepeatingS(str);
+    }
+}
